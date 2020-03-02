efebf978241fb5a5a01218078599ebcf6f0b2a0d
scip92 <scipm5592@gmail.com>
2020-03-02 22:42:39 +0100

    Cleanup http client

--- src/api/http-client.ts
+++ src/api/http-client.ts
@@ -1,16 +1,16 @@
-import axios, { AxiosInstance } from "axios";
+import axios, {AxiosInstance, AxiosResponse} from "axios";
 
 const baseUrl = "https://classicguildbankapi.azurewebsites.net/api";
 
 export const createHttpClient = (apiToken: string = null): AxiosInstance => {
     let headers = {};
     if (apiToken) {
-        headers = { "Authorization": `Bearer ${apiToken}` }
+        headers = {"Authorization": `Bearer ${apiToken}`}
     }
-    const client = axios.create({ baseURL: this.baseUrl, headers });
+    const client = axios.create({baseURL: baseUrl, headers});
     client.interceptors.request.use(request => {
-        console.log({ request });
+        console.log({request});
         return request
-    })
+    });
     return client;
-}
+};
