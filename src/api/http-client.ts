0030f96a97895363c5bc6816ff7de614622fc38d
scip92 <scipm5592@gmail.com>
2020-03-30 22:36:29 +0200

    Improve logging for http requests

--- src/api/http-client.ts
+++ src/api/http-client.ts
@@ -9,8 +9,12 @@ export const createHttpClient = (apiToken: string = null): AxiosInstance => {
     }
     const client = axios.create({baseURL: baseUrl, headers});
     client.interceptors.request.use(request => {
-        console.log({request});
+        console.log(`[${new Date().toISOString()}] Request: ${request.url}`);
         return request
+    });
+    client.interceptors.response.use(response => {
+        console.log(`[${new Date().toISOString()}] Response: ${response.status}`);
+        return response;
     });
     return client;
 };
